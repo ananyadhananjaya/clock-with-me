71ea7bd28708bc794e76e9b9b28d991fef92993d
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-06-19 13:34:38 +0530

    chore: setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+ <Hello name="Ananya" />
+</template>
+
+<style>
+html{
+  background: #222;
+  columns: #fff;
+  color-scheme: dark;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+    name: string
+}>()
+</script>
+
+<template>
+  Hello {{name}}
+</template>
+
+
